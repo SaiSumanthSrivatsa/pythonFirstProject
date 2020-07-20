a8da410c3828d07b694c05c2f91c27daff36ba6f
SaiSumanthSrivatsa <68456955+SaiSumanthSrivatsa@users.noreply.github.com>
2020-07-20 19:55:21 +0530

    Updated the code for dynamic length

--- password.py
+++ password.py
@@ -1,5 +1,5 @@
 import random
 s="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()"
-pwdlen=16
+pwdlen=input("Enter the length of the password dynamically")
 pwd="".join(random.sample(s,pwdlen))
 print(pwd)
